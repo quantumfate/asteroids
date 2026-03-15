373bbce1ed2cb4ae4cad8f9d2f47690037f9ba9a
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-03-15 20:38:31 +0100

    Reduced CPU usage

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,6 +22,8 @@ def main():
 
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
