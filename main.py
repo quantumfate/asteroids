807a9670dc0d9c49e818c7e0f4f03b5ab51d9fdd
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-03-15 21:32:23 +0100

    Organizing Players into Groups

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
@@ -24,8 +28,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
